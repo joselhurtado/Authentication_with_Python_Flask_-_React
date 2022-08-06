be1f6764108f40925ce43e26df491e3e5c154ffb
Jose Hurtado <hello@hurtadojose.com>
2022-08-06 15:15:57 +0000

    Login page WIP

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -7,13 +7,31 @@ export const Login = () => {
 	const { email, setEmail} = useState("");
 	const { password, setPassword} = useState("");
 
+const handleClick = () => {
+	const opts = {
+		method: 'POST',
+		body: JSON.stringify({
+			"email": email,
+			"password": password
+		})
+	}
+	fetch('https://3000-4geeksacade-reactflaskh-xpm8eu1lzpr.ws-us59.gitpod.io/api/token', opts)
+		.then(resp => {
+			if(resp.status === 200) return resp.json();
+			else alert('There has been some error');
+		})
+		.then()
+		.catch(error =>
+			console.error('There was an Error!!!', error));
+}
+
 	return (
 		<div className="text-center mt-5">
 			<h1>LOGIN</h1>
 			<div>
-				<input type='text' placeholder='Your Email' onChange={() => setEmail(e.target.value)} />
-				<input type='password' placeholder='Your Password' onChange={() => setPassword(e.target.value)} />
-				<div className='btn btn-primary' >Login</div>
+				<input type='text' placeholder='Your Email' value={email} onChange={() => setEmail(e.target.value)} />
+				<input type='password' placeholder='Your Password' value={password} onChange={() => setPassword(e.target.value)} />
+				<div className='btn btn-primary' onClick={handleClick}>Login</div>
 			</div>
 		</div>
 	);
